e5c33a96bbd2e13c630a65047fc9ed6e41825a6f
Douglas Lincoln <102931376+Douglas-Correia@users.noreply.github.com>
2023-10-11 05:19:31 -0400

    Add files via upload

---
+++ js/catalogo.js
@@ -0,0 +1,47 @@
+export const produtosCatalogo = [
+    {
+        id: 1,
+        imagem: '../img/Iphones.jpg',
+        nome: 'Produto 1',
+        descricao: 'Descrição do Produto 1',
+        valor: 19.99,
+        tipo: 'celular',
+        marca: 'Samsung'
+    },
+    {
+        id: 2,
+        imagem: '../img/Iphones.jpg',
+        nome: 'Produto 2',
+        descricao: 'Descrição do Produto 2',
+        valor: 29.99,
+        tipo: 'celular',
+        marca: 'Motorola'
+    },
+    {
+        id: 3,
+        imagem: '../img/Iphones.jpg',
+        nome: 'Produto 2',
+        descricao: 'Descrição do Produto 3',
+        valor: 29.99,
+        tipo: 'celular',
+        marca: 'LG'
+    },
+    {
+        id: 4,
+        imagem: '../img/Iphones.jpg',
+        nome: 'Produto 4',
+        descricao: 'Descrição do Produto 4',
+    },
+    {
+        id: 5,
+        imagem: '../img/Iphones.jpg',
+        nome: 'Produto 5',
+        descricao: 'Descrição do Produto 5',
+    },
+    {
+        id: 6,
+        imagem: '../img/Iphones.jpg',
+        nome: 'Produto 6',
+        descricao: 'Descrição do Produto 6',
+    },
+];
