1189b77f79edfcd46f3c9c357657951acf64b001
Douglas Lincoln <102931376+Douglas-Correia@users.noreply.github.com>
2023-10-17 04:56:04 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
+import { adicionarCarrinho } from "./carrinho.js";
 import { produtosCatalogo } from "./catalogo.js";
 
-function renderizarCatalogo(){
+export function renderizarCatalogo() {
     for(const produto of produtosCatalogo){
         const cardProdutos = `
         <div class="produto-card">
@@ -9,32 +10,48 @@ function renderizarCatalogo(){
         <p class="produto-text">${produto.descricao}</p>
         <p class="produto-text">${produto.valor}.00</p>
         <button id="adicionar-${produto.id}" class="produto-button"><i class="fa-solid fa-cart-plus"></i> Adicionar ao Carrinho</button>
-        </div>`
+        </div>`;
 
         document.querySelector("#container-produtos").innerHTML += cardProdutos;
+    }
+
+    for(const produto of produtosCatalogo){
+        document.querySelector(`#adicionar-${produto.id}`).addEventListener('click', () =>{
+            adicionarCarrinho(produto.id);
+        })
+    }
 }
+
+export function abrirMenuFecharMenu(){
+    // IDS para o menu
+    const menuToggle = document.querySelector('#menu-toggle');
+    const exitMenu = document.querySelector('#exit-menu');
+    const navMenu = document.querySelector('#nav-menu');
+
+    menuToggle.addEventListener('click', () =>{
+        navMenu.classList.remove('hide');
+        navMenu.classList.add('menu-bar');
+    })
+
+    exitMenu.addEventListener('click', () =>{
+        navMenu.classList.remove('menu-bar');
+        navMenu.classList.add('hide');
+    })
 }
 
-renderizarCatalogo();
+export function abrirCarrinhoFecharCarrinho(){
+    // IDS para o carrinho 
+    const idCart = document.querySelector('#cart');
+    const exitCart = document.querySelector('#exit-cart');
+    const idCartComperContainer = document.querySelector('#container-cart');
 
-// IDS para o menu
-const menuToggle = document.querySelector('#menu-toggle');
-const exitMenu = document.querySelector('#exit-menu');
-const navMenu = document.querySelector('#nav-menu');
+    idCart.addEventListener('click', () => {
+        idCartComperContainer.classList.remove('hide');
+        idCartComperContainer.classList.add('container-cart');
+    });
 
-menuToggle.addEventListener('click', () =>{
-    navMenu.classList.remove('hide');
-    navMenu.classList.add('menu-bar');
-})
-
-exitMenu.addEventListener('click', () =>{
-    navMenu.classList.remove('menu-bar');
-    navMenu.classList.add('hide');
-})
-
-const idCart = document.querySelector('#cart');
-const idCartComperContainer = document.querySelector('#cart-comper');
-
-idCart.addEventListener('click', () => {
-    idCartComperContainer.style.display = 'block'; // Mostrar o contêiner do carrinho
-});
+    exitCart.addEventListener('click', () =>{
+        idCartComperContainer.classList.remove('container-cart');
+        idCartComperContainer.classList.add('hide');
+    })
+}
